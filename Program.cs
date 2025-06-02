7bac3fecc0f6a6e05093db91095c4cd25d691c15
Luiz Fernando <odnanreffreitas641@gmail.com>
2025-06-02 15:36:26 -0300

    Ajuste para exibir a exception no console.

--- Program.cs
+++ Program.cs
@@ -18,7 +18,17 @@ Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 100);
 // Cria uma nova reserva, passando a suíte e os hóspedes
 Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+
+
+try
+{
+    reserva.CadastrarHospedes(hospedes);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro: {ex.Message}");
+    return;
+}
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
